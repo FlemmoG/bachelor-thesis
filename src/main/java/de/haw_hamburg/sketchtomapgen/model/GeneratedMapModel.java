23378aa092147581434e3d32f1b79832be9e7087
ada020 <flemming.grabowski@haw-hamburg.de>
2025-01-28 14:54:17 +0100

    WIP

--- src/main/java/de/haw_hamburg/sketchtomapgen/model/GeneratedMapModel.java
+++ src/main/java/de/haw_hamburg/sketchtomapgen/model/GeneratedMapModel.java
@@ -38,6 +38,10 @@ public class GeneratedMapModel {
     return writableImage;
   }
 
+  public boolean isWithinBounds(int x, int y) {
+    return x >= 0 && x < width && y >= 0 && y < height;
+  }
+
   public int getHeight() {
     return height;
   }
